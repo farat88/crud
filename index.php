8c980cd4988ee5e381889a159777896d7fe4f5a5
ХитрыйДжони <56193451+farat88@users.noreply.github.com>
2021-12-17 11:32:11 +0300

    adding styles to the table

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php
 require_once 'connect.php';
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<style>
+    th, td {
+        padding: 10px;
+    }
+    th {
+        background: #606060;
+        color: white;
+    }
+    td {
+        background: #b5b5b5;
+        color: black;
+    }
+</style>
+<body>
+<table>
+<tr>
+    <th>ID</th>
+    <th>Title</th>
+    <th>Description</th>
+    <th>Price</th>
+    <tr>
+        <td>1</td>
+        <td>Product 1</td>
+        <td>Lorem ipsun</td>
+        <td>1000$</td>
+    </tr>
+</tr>
+</table>
+</body>
+</html>
